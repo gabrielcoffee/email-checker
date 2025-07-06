015aed91d1b62ca8cbffe84b3804e2271571a270
gabrielcoffee <cafegabriel1@gmail.com>
2025-07-06 10:48:47 -0400

    removed debug mode

--- api.py
+++ api.py
@@ -44,4 +44,4 @@ def receive_file():
 # Rodar a aplicação
 if __name__ == '__main__':
     print("API rodando em: http://localhost:5001")
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)
