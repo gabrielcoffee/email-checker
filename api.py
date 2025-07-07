dc5e8a45c8411d5748d148f3bbcdb168e868b60a
gabrielcoffee <cafegabriel1@gmail.com>
2025-07-06 21:52:52 -0400

    remocao de rota health

--- api.py
+++ api.py
@@ -19,10 +19,6 @@ def index():
         print(f"Erro ao renderizar template: {e}")
         return f"Erro: {e}", 500
 
-# Rota de health check para o Render
-@app.route('/health', methods=['GET'])
-def health_check():
-    return jsonify({'status': 'healthy', 'message': 'API funcionando!'})
 
 # Rota para receber texto e retornar uma resposta
 @app.route('/text', methods=['POST'])
